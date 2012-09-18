ecd949d78c0c559113b5c4b22f15f50bfa62505c
Pete X. Graham <pete.graham@piratalondon.com>
2012-09-18 18:30:30 +0100

    Admin showing now

--- pxg_blog/blog/models.py
+++ pxg_blog/blog/models.py
@@ -1,3 +1,19 @@
+#from .utils import slugify
+from datetime import datetime
+from django.contrib.auth.models import User
+from django.contrib.sitemaps import Sitemap
 from django.db import models
+from django.db.models import permalink
+#from minicms.models import BaseContent
+from random import choice
+from string import ascii_letters, digits
+import re
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    body = models.CharField(max_length=5000)
+
+    # System fields
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
